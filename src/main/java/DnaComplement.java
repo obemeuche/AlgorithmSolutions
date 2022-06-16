a77862b16637d1e4c05c9dac35331c9553131a7d
obemeuche <obemeuchechi@gmail.com>
2022-06-16 23:04:01 +0100

    DnaComplement question and solution

--- src/main/java/DnaComplement.java
+++ src/main/java/DnaComplement.java
@@ -1,6 +1,18 @@
 import java.util.Arrays;
 
 public class DnaComplement {
+
+//    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G".
+//
+//    Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side.
+//
+//    DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+//    Example: (input --> output)
+//
+//            "ATTGC" --> "TAACG"
+//            "GTAT" --> "CATA"
+
         public static String DnaComplement(String dna)
         {
             char [] newArr = dna.toCharArray();
@@ -20,7 +32,7 @@ public class DnaComplement {
             }
 
 
-
+                    // OR
 
 //            char[] charArray = dna.toCharArray();
 //
