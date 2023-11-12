27ac6312846e048e6059df9f84e3a75b3c87d90c
obemeuche <obemeuchechi@gmail.com>
2023-11-12 11:01:45 +0100

    feat/implemented a demo on insertion sort

---
+++ src/main/java/InsertionSortDemo.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class InsertionSortDemo {
+    public static void main(String[] args) {
+        int [] arr = {2,4,3,1,6,5,7,0};
+
+        int [] sortedArray = insertionSort(arr);
+
+        System.out.println("Inserted Array: " + Arrays.toString(sortedArray));
+    }
+
+    private static int[] insertionSort(int[] arr) {
+        int steps = 0;
+
+        for(int i = 1; i < arr.length; i++){
+            steps++;
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        System.out.println("Steps: " + steps);
+        return arr;
+    }
+}
