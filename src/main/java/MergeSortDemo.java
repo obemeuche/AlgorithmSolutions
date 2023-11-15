2458a3858bec86fe6af28113e9e3184170837460
obemeuche <obemeuchechi@gmail.com>
2023-11-16 00:47:44 +0100

    feat/implementing a demo on merge sort

--- src/main/java/MergeSortDemo.java
+++ src/main/java/MergeSortDemo.java
@@ -1,2 +1,37 @@
+import java.util.Arrays;
+
 public class MergeSortDemo {
+
+    private static int[] mergeSort(int[] arr, int l, int r)
+    {
+        if(l < r)
+        {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m+1, r);
+
+            merge(arr, l, m, r);
+        }
+
+        return arr;
+    }
+
+    private static void merge(int[] arr, int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,7,2,9,5,8,4};
+
+        int[] sortedArray = mergeSort(arr, 0, arr.length-1);
+
+        System.out.println("Sorted Array: " + Arrays.toString(sortedArray));
+    }
+
 }
