880b06a0244dd14e105118579e7a6ac2d12f5094
obemeuche <obemeuchechi@gmail.com>
2023-11-21 13:24:28 +0100

    feat/added a reversal method on Singly LinkedList

--- src/main/java/SingleLinkedListDemo.java
+++ src/main/java/SingleLinkedListDemo.java
@@ -8,23 +8,34 @@ public class SingleLinkedListDemo {
     // it uses the 0(n) for searching.
 
     static Node head;
-
-    static class Node {
+    static class Node
+    {
         int data;
         Node next;
     }
 
-    public static void main(String[] args) {
-
-        int data = 5;
-        insertToLinkedList(data);
+    public static void main(String[] args)
+    {
+        insertToLinkedList(5);
         insertToLinkedList(3);
         insertToLinkedList(2);
+        insertToLinkedList(10);
 
         viewList();
-    }
-    private static void insertToLinkedList(int data) {
 
+        Node node = head;
+        Node reversedNode = reverseList(node);
+
+        while (reversedNode != null)
+        {
+            System.out.println(reversedNode.data);
+            reversedNode = reversedNode.next;
+        }
+
+    }
+
+    private static void insertToLinkedList(int data)
+    {
         Node node = new Node();
         node.data = data;
         node.next = null;
@@ -43,15 +54,32 @@ public class SingleLinkedListDemo {
         }
     }
 
-    private static void viewList() {
-
+    private static void viewList()
+    {
         Node node = head;
         while(node.next != null)
         {
-            System.out.println(node.data);
+//            System.out.println(node.data);
             node = node.next;
         }
-        System.out.println(node.data);
+//        System.out.println(node.data);
+    }
+
+    private static Node reverseList(Node node) {
+        Node previousNode = null;
+        Node currentNode = node;
+        Node nextNode = null;
+
+        while (currentNode.next != null)
+        {
+            nextNode = currentNode.next;
+            currentNode.next = previousNode;
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        node = previousNode;
+
+        return node;
     }
 }
 
